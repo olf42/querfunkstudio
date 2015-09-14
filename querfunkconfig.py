fd66c8cf798632eecca6becf581d99ec9cc57db6
olf <olf@subsignal.org>
2015-09-14 19:21:55 +0200

    separated config into separate file

---
+++ querfunkconfig.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os.path
+
+DATABASE_DIR = "database"
+DATABASE_FILE = "querfunkstudio_development.db"
+DATABASE = os.path.join(os.path.dirname(__file__), DATABASE_DIR, DATABASE_FILE)
+
+LOG_TYPE = {0:"info",
+            1:"warning",
+            2:"error"}
+LOG_DISPLAY = {0:"success",
+               1:"warning",
+               2:"danger"}
+
+ERROR_LOGIN_MSG = "Username not found, Password mismatch or user not active"
+ERROR_SESSION_MSG = "No Username found in your Session. Please log in."
+ERROR_USERCREATE_MSG = "User already exists! Choose a different Name and try again."
+ERROR_PASSWORD_MSG = "Passwords don't match. Try again!"
+ERROR_NOTSUPERUSER_MSG = "User is not superuser!"
+
+SUCCESS_REGISTRATION_MSG = "Account created. Needs approval."
--- querfunkstudio.py
+++ querfunkstudio.py
@@ -8,23 +8,8 @@ import random
 from hashlib import sha512
 from jinja2 import Environment, PackageLoader
 
-DATABASE_DIR = "database"
-DATABASE_FILE = "querfunkstudio_development.db"
-DATABASE = os.path.join(os.path.dirname(__file__), DATABASE_DIR, DATABASE_FILE)
-LOG_TYPE = {0:"info",
-            1:"warning",
-            2:"error"}
-LOG_DISPLAY = {0:"success",
-               1:"warning",
-               2:"danger"}
+from querfunkconfig import *
 
-ERROR_LOGIN_MSG = "Username not found, Password mismatch or user not active"
-ERROR_SESSION_MSG = "No Username found in your Session. Please log in."
-ERROR_USERCREATE_MSG = "User already exists! Choose a different Name and try again."
-ERROR_PASSWORD_MSG = "Passwords don't match. Try again!"
-ERROR_NOTSUPERUSER_MSG = "User is not superuser!"
-
-SUCCESS_REGISTRATION_MSG = "Account created. Needs approval."
 
 class Querfunkstudio(object):
 
